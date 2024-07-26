f933d0da13fdbcc9d78c9c8e09f7a1908180218f
Aurelie Moustardier <naoorelie@gmail.com>
2024-07-26 16:47:05 +0200

    Branche P1 terminée

--- P1/P1C5/énoncé/main.py
+++ P1/P1C5/énoncé/main.py
@@ -1,1 +1,16 @@
 # Écrivez votre code ici !
+nom = "Aurélie"
+age = 26
+taille = 1.7
+est_etudiant = True
+
+print(f"Mon nom : {nom}")
+print(f"Mon âge : {age}")
+print(f"Ma taille : {taille}")
+print(f"Je suis étudiante : {est_etudiant}")
+
+print(type(nom))
+print(type(age))
+print(type(taille))
+print(type(est_etudiant))
+
