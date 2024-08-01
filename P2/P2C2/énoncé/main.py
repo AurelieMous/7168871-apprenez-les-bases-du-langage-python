47f2472c9dc4ab72ad9666370732f63fe1f9e4e1
Aurelie Moustardier <naoorelie@gmail.com>
2024-08-01 22:49:50 +0200

    pc2 exos

--- P2/P2C2/énoncé/main.py
+++ P2/P2C2/énoncé/main.py
@@ -1,8 +1,27 @@
+
 def main():
+    nombre = input("Liste de nombre entier séparé par des virgules :")
+    liste_nombre = nombre.split(", ")
+    print(liste_nombre)
+    
+    resultat = 0
+    for addition in liste_nombre:
+        resultat += int(addition)
+        print(f"Somme des nombres : {resultat}")
+
+    moyenne = resultat / len(liste_nombre)
+    print(f'Moyenne des nombres : {moyenne}')
+
+    for addition in liste_nombre:
+        supperieur_moyenne = addition >= moyenne
+        print(f"Nombre de nombres supérieurs à la moyenne : {supperieur_moyenne}")
+   
+    
+        
     # Ecrivez votre code ici !
     # Attention tout votre code doit être indenté comme ce commentaire
 
 
 # Ne touchez pas le code ci-dessous
 if __name__ == "__main__":
-    main()
+    main() 
