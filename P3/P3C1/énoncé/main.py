48b938b74294e9349f6c0ad515bf95a248ae2e20
Aurelie Moustardier <naoorelie@gmail.com>
2024-08-05 16:12:13 +0200

    P3C1

--- P3/P3C1/énoncé/main.py
+++ P3/P3C1/énoncé/main.py
@@ -1,6 +1,12 @@
 # Ecrivez votre code ici !
+import operations
 
+def main():
+    resultat_addition = operations.addition(3, 5)
+    resultat_multiplication = operations.multiplication(8, 3)
 
+    print(resultat_addition)
+    print(resultat_multiplication)
 # Ne touchez pas le code ci-dessous
 if __name__ == "__main__":
     main()
--- P3/P3C1/énoncé/operations.py
+++ P3/P3C1/énoncé/operations.py
@@ -1,1 +1,8 @@
 # Écrivez votre code ici !
+def addition(a, b):
+    somme = a + b
+    return somme
+
+def multiplication(a, b):
+    produit = a * b
+    return produit
